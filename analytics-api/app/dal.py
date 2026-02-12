1fdb49ad2604a99ee18e2a7245fdf2090d1e0dd2
jacob sun <jacobsun211@gmail.com>
2026-02-12 12:57:36 +0200

    endpoint number 2

--- analytics-api/app/dal.py
+++ analytics-api/app/dal.py
@@ -30,3 +30,28 @@ def customers_with_most_orders():
         )
 
     return query_result
+
+
+def customers_with_no_orders():
+    sql = """
+    SELECT customerNumber ,COUNT(orderNumber) as num_of_orders
+    FROM orders 
+    GROUP BY COUNT(orderNumber)
+    HAVING num_of_orders = 0
+        """
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    table = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    query_result = []
+    for column in table:
+        query_result.append(
+            {
+                "customerNumber": column[0],
+                "num_of_orders": column[1],
+            }
+        )
+
+    return query_result
--- analytics-api/app/main.py
+++ analytics-api/app/main.py
@@ -9,3 +9,8 @@ app = FastAPI()
 @app.get("/analytics/top-customers")
 def customersmost_orders():
     return dal.customers_with_most_orders()
+
+
+@app.get("/analytics/customers-without-orders")
+def null_customers():
+    return dal.customers_with_no_orders()
